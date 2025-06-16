8ff04258d3453fc3e122fd3f3e6427cfd9f8a27e
Gary Sounigo <gary@great-solution.fr>
2025-06-16 23:34:13 +0200

    change Ollama client for the default ollama client, change message to fit Message[] type from ollama

--- server/src/app/chat.service.ts
+++ server/src/app/chat.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@nestjs/common';
 //  GoogleGenerativeAI,
 //} from '@google/generative-ai';
 
-import { Ollama } from 'ollama'
+import  ollama from 'ollama';
 
 import { ChatContent } from 'data-model';
 
@@ -14,20 +14,21 @@ export class ChatService {
   //model: GenerativeModel;
   //chatSession: ChatSession;
   ollama_url: string = "http://localhost:11434";
-  ollama_client: any = new Ollama({'host': this.ollama_url });
-  history: ChatContent[] = [
-    {
-      role: 'user',
-      message: `You're a poet. Respond to all questions with a rhyming poem.
-        What is the capital of California?
-      `,
-    },
-    {
-      role: 'system',
-      message:
-        'If the capital of California is what you seek, Sacramento is where you ought to peek.',
-    },
-  ];
+  //ollama_client: any = new Ollama({'host': this.ollama_url });
+  //history: ChatContent[] = [];
+  // history: ChatContent[] = [
+  //   {
+  //     role: 'user',
+  //     message: `You're a poet. Respond to all questions with a rhyming poem.
+  //       What is the capital of California?
+  //     `,
+  //   },
+  //   {
+  //     role: 'system',
+  //     message:
+  //       'If the capital of California is what you seek, Sacramento is where you ought to peek.',
+  //   },
+  // ];
 
   constructor() {
     //const genAI = new GoogleGenerativeAI(process.env.API_KEY);
@@ -61,14 +62,16 @@ export class ChatService {
 
     const { agent, message } = chatContent;
     const role = agent == 'user' ? agent : 'user';  
-    this.history.push({role, message});
-     console.log(this.history) 
-    const response = await this.ollama_client.chat({
+    //this.history.push({role, message});
+     //console.log(this.history) 
+
+    const response = await ollama.chat({
       model: "llama3.2:1b",
-      messages: this.history,
+      messages: [{role, content: message}],
       stream: false
     });
 
+    console.log('response', response)
     return {
       message: response.message.content,
       role: 'assistant',
