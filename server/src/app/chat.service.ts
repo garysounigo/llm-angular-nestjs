eb8293780ffe48cd5af7c1157f512478592703c5
Gary Sounigo <gary@great-solution.fr>
2025-06-16 22:40:27 +0200

    change server chat.service to migrate gemini to ollama

--- server/src/app/chat.service.ts
+++ server/src/app/chat.service.ts
@@ -1,44 +1,79 @@
 import { Injectable } from '@nestjs/common';
-import {
-  ChatSession,
-  GenerativeModel,
-  GoogleGenerativeAI,
-} from '@google/generative-ai';
+//import {
+//  ChatSession,
+//  GenerativeModel,
+//  GoogleGenerativeAI,
+//} from '@google/generative-ai';
+
+import { Ollama } from 'ollama'
 
 import { ChatContent } from 'data-model';
 
 @Injectable()
 export class ChatService {
-  model: GenerativeModel;
-  chatSession: ChatSession;
+  //model: GenerativeModel;
+  //chatSession: ChatSession;
+  ollama_url: string = "http://localhost:11434";
+  ollama_client: any = new Ollama({'host': this.ollama_url });
+  history: ChatContent[] = [
+    {
+      role: 'user',
+      message: `You're a poet. Respond to all questions with a rhyming poem.
+        What is the capital of California?
+      `,
+    },
+    {
+      role: 'system',
+      message:
+        'If the capital of California is what you seek, Sacramento is where you ought to peek.',
+    },
+  ],
+
   constructor() {
-    const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-    this.model = genAI.getGenerativeModel({ model: 'gemini-pro' });
-    this.chatSession = this.model.startChat({
-      history: [
-        {
-          role: 'user',
-          parts: `You're a poet. Respond to all questions with a rhyming poem.
-            What is the capital of California?
-          `,
-        },
-        {
-          role: 'model',
-          parts:
-            'If the capital of California is what you seek, Sacramento is where you ought to peek.',
-        },
-      ],
-    });
+    //const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+    //this.model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+    //this.chatSession = this.model.startChat({
+    //  history: [
+    //    {
+    //      role: 'user',
+    //      parts: `You're a poet. Respond to all questions with a rhyming poem.
+    //        What is the capital of California?
+    //      `,
+    //    },
+    //    {
+    //      role: 'model',
+    //      parts:
+    //        'If the capital of California is what you seek, Sacramento is where you ought to peek.',
+    //    },
+    //  ],
+    //});
   }
 
   async chat(chatContent: ChatContent): Promise<ChatContent> {
-    const result = await this.chatSession.sendMessage(chatContent.message);
-    const response = await result.response;
-    const text = response.text();
+    //const result = await this.chatSession.sendMessage(chatContent.message);
+    //const response = await result.response;
+    //const text = response.text();
+
+    //return {
+    //  message: text,
+    //  agent: 'chatbot',
+    //};
+
+    const { agent, message } = chatContent;
+    const role = agent == 'user' ? agent : 'user';  
+    this.history.push({role, message});
+    
+    const response = await ollama_client.chat({
+      model: "llama3.2:1b",
+      messages: this.history,
+      stream: false
+    });
 
     return {
-      message: text,
-      agent: 'chatbot',
-    };
+      message: response.message.content,
+      role: 'assistant'
+      agent: 'chatbot'
+    }
+
   }
 }
